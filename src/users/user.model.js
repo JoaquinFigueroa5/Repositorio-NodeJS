47ce098c4c9d6f64aeae39fcd9d85b56c6ddae18
JoaquinFigueroa5 <hfigueroa-2023417@kinal.edu.gt>
2025-01-27 17:29:50 -0600

    Se creo el servidor

---
+++ src/users/user.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    correo: {
+        type: String,
+        required: [true, 'El correo es obligatorio'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatoria']
+    },
+    img: {
+        type: String,
+        
+    },
+    phone: {
+        type: String,
+        minLenght: 8,
+        maxLenght: 8,
+        required: [true, 'El telefono es obligatorio']
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['ADMIN_ROLE', 'USER_ROLE']
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+UserSchema.methods.toJSON = function(){
+    const { __v, password, _id, ...usuario} = this.toObject();
+    usuario.uid = _id;
+    return usuario;
+}
+
+export default mongoose.model('User', UserSchema);
